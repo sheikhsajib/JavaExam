b8b3fc30c01f5013f9a84234bd782ef8a863ca50
SHEIKH SAJIB <45914914+sheikhsajib@users.noreply.github.com>
2022-08-21 12:24:39 +0600

    gcd

---
+++ GretestCommonDivisors.java
@@ -0,0 +1,25 @@
+package maxmintest;
+
+import java.util.Scanner;
+
+public class GretestCommonDivisors {
+
+    public static void main(String[] args) {
+    // find GCD between n1 and n2
+    Scanner input = new Scanner(System.in);
+        System.out.println("Number 1 : ");
+        int n1 = input.nextInt();
+    
+        System.out.println("Number 2 : ");
+        int n2 = input.nextInt();
+
+        int gcd = 1;   // initially set to gcd
+
+    for (int i = 1; i <= n1 && i <= n2; ++i) {
+      // check if i perfectly divides both n1 and n2
+      if (n1 % i == 0 && n2 % i == 0)
+        gcd = i;
+    }
+    System.out.println("GCD of " + n1 +" and " + n2 + " is " + gcd);
+    }
+}
